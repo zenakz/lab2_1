2056c95f258f34bc4162b4cc89a5097596cd0caf
radoslaw.adamus <radoslaw.adamus@p.lodz.pl>
2019-03-19 06:04:45 +0100

    private constructor added

--- src/main/java/edu/iis/mto/bsearch/BinarySearch.java
+++ src/main/java/edu/iis/mto/bsearch/BinarySearch.java
@@ -1,45 +1,47 @@
 /**
- * 
+ *
  */
 package edu.iis.mto.bsearch;
 
 /**
  * Utility Class dla wyszukiwania binarnego
- * 
+ *
  */
 public class BinarySearch {
 
-	/**
-	 * Metoda realizujaca wyszukiwanie binarne
-	 * 
-	 * @param key
-	 *            - szukany obiekt
-	 * @param seq
-	 *            - rosnaco uporzadkowana niepusta sekwencja
-	 * @return obiekt rezultatu o polach: - found (true jezeli znaleziony) -
-	 *         position (jezeli znaleziony - pozycja w sekwencji, jezeli nie
-	 *         znaleziony -1)
-	 */
-	public static SearchResult search(int key, int[] seq) {
-		int start = 0;
-		int end = seq.length - 1;
-		int center;
-		SearchResult result = new SearchResult();
+    private BinarySearch() {}
 
-		while (start <= end) {
-			center = (start + end) / 2;
-			if (seq[center] == key) {
-				result.setPosition(center + 1);
-				break;
-			} else {
-				if (seq[center] < key)
-					start = center + 1;
-				else
-					end = center - 1;
-			}
+    /**
+     * Metoda realizujaca wyszukiwanie binarne
+     *
+     * @param key
+     *            - szukany obiekt
+     * @param seq
+     *            - rosnaco uporzadkowana niepusta sekwencja
+     * @return obiekt rezultatu o polach: - found (true jezeli znaleziony) - position (jezeli znaleziony - pozycja w
+     *         sekwencji, jezeli nie znaleziony -1)
+     */
+    public static SearchResult search(int key, int[] seq) {
+        int start = 0;
+        int end = seq.length - 1;
+        int center;
+        SearchResult result = new SearchResult();
 
-		}
-		return result;
-	}
+        while (start <= end) {
+            center = (start + end) / 2;
+            if (seq[center] == key) {
+                result.setPosition(center + 1);
+                break;
+            } else {
+                if (seq[center] < key) {
+                    start = center + 1;
+                } else {
+                    end = center - 1;
+                }
+            }
+
+        }
+        return result;
+    }
 
 }
